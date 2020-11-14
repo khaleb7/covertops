db2bde0bb9b746ee4bf4e0d87aac99da6e344c1e
khaleb7 <josh.rasey@gmail.com!>
2020-11-14 16:53:07 -0600

    SPlitting Warrior/Marital Artist

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -38,20 +38,18 @@ export class covertOpsActor extends Actor {
     } else if (data.skills.academic.primSec == "S") {
       data.skills.academic.base = Math.ceil(Number(data.abilities.log.value) / 2) + 10;
     } else {
-      data.skills.academic.base = Math.ceil(Number(data.abilities.log.value) / 2)
+      data.skills.academic.base = Math.ceil(Number(data.abilities.log.value) / 2);
     }
-    ;
     data.skills.academic.value = Number(data.skills.academic.base) + Number(10 * data.skills.academic.level);
-    ;
+
 
     if (data.skills.investigator.primSec == "P") {
       data.skills.investigator.base = Math.ceil(Number(data.abilities.log.value) / 2) + 20;
     } else if (data.skills.investigator.primSec == "S") {
       data.skills.investigator.base = Math.ceil(Number(data.abilities.log.value) / 2) + 10;
     } else {
-      data.skills.investigator.base = Math.ceil(Number(data.abilities.log.value) / 2)
+      data.skills.investigator.base = Math.ceil(Number(data.abilities.log.value) / 2);
     }
-    ;
     data.skills.investigator.value = Number(data.skills.investigator.base) + Number(10 * data.skills.investigator.level);
 
     if (data.skills.leader.primSec == "P") {
@@ -59,26 +57,25 @@ export class covertOpsActor extends Actor {
     } else if (data.skills.leader.primSec == "S") {
       data.skills.leader.base = Math.ceil(Number(data.abilities.wil.value) / 2) + 10;
     } else {
-      data.skills.leader.base = Math.ceil(Number(data.abilities.wil.value) / 2)
+      data.skills.leader.base = Math.ceil(Number(data.abilities.wil.value) / 2);
     }
-    ;
+
     if (data.skills.leader.level > 0) {
-      data.skills.leader.value = Number(data.skills.leader.base) + Number(10 * data.skills.leader.level)
+      data.skills.leader.value = Number(data.skills.leader.base) + Number(10 * data.skills.leader.level);
     }
-    ;
+
 
     if (data.skills.jedi.primSec == "P") {
       data.skills.jedi.base = Math.ceil(Number(data.abilities.wil.value) / 2) + 20;
     } else if (data.skills.jedi.primSec == "S") {
       data.skills.jedi.base = Math.ceil(Number(data.abilities.wil.value) / 2) + 10;
     } else {
-      data.skills.jedi.base = Math.ceil(Number(data.abilities.wil.value) / 2)
+      data.skills.jedi.base = Math.ceil(Number(data.abilities.wil.value) / 2);
     }
-    ;
+
     if (data.skills.jedi.level > 0) {
-      data.skills.jedi.value = Number(data.skills.jedi.base) + Number(10 * data.skills.jedi.level)
+      data.skills.jedi.value = Number(data.skills.jedi.base) + Number(10 * data.skills.jedi.level);
     }
-    ;
 
     if (data.skills.sith.primSec == "P") {
       data.skills.sith.base = Math.ceil(Number(data.abilities.wil.value) / 2) + 20;
@@ -86,34 +83,32 @@ export class covertOpsActor extends Actor {
       data.skills.sith.base = Math.ceil(Number(data.abilities.wil.value) / 2) + 10;
     } else {
       data.skills.sith.base = Math.ceil(Number(data.abilities.wil.value) / 2)
-    }
-    ;
+    };
     if (data.skills.sith.level > 0) {
-      data.skills.sith.value = Number(data.skills.sith.base) + Number(10 * data.skills.sith.level)
+      data.skills.sith.value = Number(data.skills.sith.base) + Number(10 * data.skills.sith.level);
     }
-    ;
+
 
     if (data.skills.medic.primSec == "P") {
       data.skills.medic.base = Math.ceil(Number(data.abilities.log.value) / 2) + 20;
     } else if (data.skills.medic.primSec == "S") {
       data.skills.medic.base = Math.ceil(Number(data.abilities.log.value) / 2) + 10;
     } else {
-      data.skills.medic.base = Math.ceil(Number(data.abilities.log.value) / 2)
+      data.skills.medic.base = Math.ceil(Number(data.abilities.log.value) / 2);
     }
-    ;
+
     if (data.skills.medic.level > 0) {
-      data.skills.medic.value = Number(data.skills.medic.base) + Number(10 * data.skills.medic.level)
+      data.skills.medic.value = Number(data.skills.medic.base) + Number(10 * data.skills.medic.level);
     }
-    ;
+
 
     if (data.skills.scout.primSec == "P") {
       data.skills.scout.base = Math.ceil(Number(data.abilities.log.value) / 2) + 20;
     } else if (data.skills.scout.primSec == "S") {
       data.skills.scout.base = Math.ceil(Number(data.abilities.log.value) / 2) + 10;
     } else {
-      data.skills.scout.base = Math.ceil(Number(data.abilities.log.value) / 2)
+      data.skills.scout.base = Math.ceil(Number(data.abilities.log.value) / 2);
     }
-    ;
     data.skills.scout.value = Number(data.skills.scout.base) + Number(10 * data.skills.scout.level);
 
     if (data.skills.soldier.primSec == "P") {
@@ -121,9 +116,9 @@ export class covertOpsActor extends Actor {
     } else if (data.skills.soldier.primSec == "S") {
       data.skills.soldier.base = Math.ceil(Number(data.abilities.dex.value) / 2) + 10;
     } else {
-      data.skills.soldier.base = Math.ceil(Number(data.abilities.dex.value) / 2)
+      data.skills.soldier.base = Math.ceil(Number(data.abilities.dex.value) / 2);
     }
-    ;
+
     data.skills.warrior.value = Number(data.skills.warrior.base) + Number(10 * data.skills.soldier.level);
 
     if (data.skills.warrior.primSec == "P") {
@@ -131,9 +126,9 @@ export class covertOpsActor extends Actor {
     } else if (data.skills.warrior.primSec == "S") {
       data.skills.warrior.base = Math.ceil(Number(data.abilities.str.value) / 2) + 10;
     } else {
-      data.skills.warrior.base = Math.ceil(Number(data.abilities.str.value) / 2)
+      data.skills.warrior.base = Math.ceil(Number(data.abilities.str.value) / 2);
     }
-    ;
+
     data.skills.warrior.value = Number(data.skills.warrior.base) + Number(10 * data.skills.warrior.level);
 
     if (data.skills.technician.primSec == "P") {
@@ -141,9 +136,9 @@ export class covertOpsActor extends Actor {
     } else if (data.skills.technician.primSec == "S") {
       data.skills.technician.base = Math.ceil(Number(data.abilities.log.value) / 2) + 10;
     } else {
-      data.skills.technician.base = Math.ceil(Number(data.abilities.log.value) / 2)
+      data.skills.technician.base = Math.ceil(Number(data.abilities.log.value) / 2);
     }
-    ;
+
     data.skills.technician.value = Number(data.skills.technician.base) + Number(10 * data.skills.technician.level);
 
     if (data.skills.thief.primSec == "P") {
@@ -151,9 +146,9 @@ export class covertOpsActor extends Actor {
     } else if (data.skills.thief.primSec == "S") {
       data.skills.thief.base = Math.ceil(Number(data.abilities.log.value) / 2) + 10;
     } else {
-      data.skills.thief.base = Math.ceil(Number(data.abilities.log.value) / 2)
+      data.skills.thief.base = Math.ceil(Number(data.abilities.log.value) / 2);
     }
-    ;
+
     data.skills.thief.value = Number(data.skills.thief.base) + Number(10 * data.skills.thief.level);
 
   }
